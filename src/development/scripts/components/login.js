33313067fced6ad2b793565618bd7200ab6976b0
Harald Floor Wilhelmsen <haraldfw@gmail.com>
2017-10-01 18:21:24 +0200

    Authentication in frontend integrated with backend. No protection of sites yet

--- src/development/scripts/components/login.js
+++ src/development/scripts/components/login.js
@@ -1,6 +1,38 @@
 import m from 'mithril'
 import style from 'style/login';
 
+let Auth = {
+    username: "",
+    password: "",
+
+    setUsername(value) {
+        Auth.username = value
+    },
+    setPassword(value) {
+        Auth.password = value
+    },
+    login() {
+        m.request({
+            url: "/api/v1/auth",
+            method: 'POST',
+            data: {username: Auth.username, password: Auth.password},
+            extract: function (xhr) {
+                return {status: xhr.status, body: xhr.responseText}
+            }
+        }).then(function (response) {
+            let status = response.status;
+            if (status === 200) {
+                let data = response.body;
+                localStorage.setItem("auth-token", data.token);
+                m.route.set("/")
+            } else {
+
+            }
+        })
+    }
+};
+
+
 export const component_name = "Login";
 export const Login = {
     view() {
@@ -10,18 +42,20 @@ export const Login = {
                 m("div.column.column-10.separator"),
                 m("div.column.column-10"),
                 m("div.column.column-40",
-                    m('form', {
+                    m('div', {
                         style: "margin-bottom: 0"
                     }, [
-                        m('label', m('input', {
-                            type: "text",
+                        m("input[type=text]", {
+                            oninput: m.withAttr("value", Auth.setUsername),
+                            value: Auth.username,
                             placeholder: "username"
-                        })),
-                        m('label', m('input', {
-                            type: "password",
+                        }),
+                        m("input[type=password]", {
+                            oninput: m.withAttr("value", Auth.setPassword),
+                            value: Auth.password,
                             placeholder: "password"
-                        })),
-                        m('input', {type: "submit", value: "Login", style: "width:100%"}),
+                        }),
+                        m("button[type=button]", {onclick: Auth.login, style: "width:100%"}, "Login")
                     ])
                 )
             )
