3d845f29ed08e2ba2aac16b7c4237057394ecfc6
Erlend <erlentob@stud.ntnu.no>
2017-11-09 00:18:49 +0100

    Working changes

--- src/development/scripts/api/reactive.js
+++ src/development/scripts/api/reactive.js
@@ -1,53 +1,15 @@
-import { Observable } from 'rxjs-es/Rx';
+import {Observable} from 'rxjs-es/Rx';
 
+const retrySeconds = 5;
 
-
-export const socket$ = Observable.create(function (obs) {
-    console.log("Opening");
-
-    function connect() {
-        const socket = new WebSocket(`ws://${window.location.host}/api/ws`);
-
-        socket.onopen = event => {
-            console.log("Open");
-            setTimeout(() => {
-                console.log("Sending");
-                socket.send(JSON.stringify({
-                    bus: "test",
-                    message: "Hello"
-                }))
-            }, 3000);
-        };
-
-        socket.onmessage = event => {
-            console.log("Message");
-            obs.next(event);
-        };
-
-        socket.onerror = event => {
-            obs.error(event);
-            console.error(event);
-        };
-
-        socket.onclose = event => {
-            function reconnect() {
-                console.log("Attemting to reconnect.");
-                const s = connect();
-                if (s == null) {
-                    setTimeout(reconnect, 1000);
-                }
-            }
-
-            reconnect();
-        };
-
-        return socket;
-    }
-
-    const socket = connect();
-
-    return () => {
-        console.log("Closing");
-        socket.close();
-    };
-}).do(x => console.log(`source: ${x}`));
+export const socket$ = Observable.webSocket(`ws://${window.location.host}/api/v1/socket`)
+    .map(message => {
+        return message;
+    })
+    .retryWhen(errors => errors
+               .do(event => {
+                   console.log(`Got error on socket restarting in ${retrySeconds}.`);
+                   console.error(event);
+               }).delay(retrySeconds * 1000))
+    .do(x => console.log(`source: ${x}`))
+    .share();
