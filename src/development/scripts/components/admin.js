6f9bf44bac406365cce54103c87f671afabbf92f
Harald Floor Wilhelmsen <harald.floor.wilhelmsen@trondheim.kommune.no>
2017-11-17 21:57:18 +0100

    Fixed typo in admin page

--- src/development/scripts/components/admin.js
+++ src/development/scripts/components/admin.js
@@ -10,7 +10,7 @@ export const Admin = site_wrapper({
         const state = this;
 
         console.log("Subscribing");
-        state.sub = changefeed('taskks').subscribe(
+        state.sub = changefeed('tasks').subscribe(
             x => console.log(`admin: ${x}`),
             err => console.error(`admin: ${err}`),
             () => console.log("admin: Completed")
