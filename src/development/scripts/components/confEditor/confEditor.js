18c72e8415e7001da1c03db051c42c4ec2750c94
Harald Floor Wilhelmsen <harald.floor.wilhelmsen@trondheim.kommune.no>
2017-12-07 14:53:36 +0100

    Config editor with textareas now works

--- src/development/scripts/components/confEditor/confEditor.js
+++ src/development/scripts/components/confEditor/confEditor.js
@@ -1,17 +1,43 @@
 import m from 'mithril';
-import {site_wrapper, dashboard_comp_wrapper} from 'scripts/site';
-import {DocEditor} from "./docEditor";
+import {map, pipe} from 'scripts/helpers/fp';
+import {site_wrapper} from "scripts/site";
+import {docEditor} from "./docEditor";
 // import style from 'style/dashboard';
 
 export const component_name = "ConfEditor";
 
+
+const State = {
+    docComps: [],
+    documents: {},
+    error: "",
+    fetch: function () {
+        const docs = State.documents;
+        m.request({
+            url: "/api/v1/documents",
+            method: 'GET',
+        }).then(function (response) {
+            for (let document of response) {
+                docs[document.name] = document;
+            }
+            State.docComps =
+                pipe(Object.keys(docs),
+                    map(d => m(docEditor(docs[d]))),
+                    Array.from);
+        }).catch(function (e) {
+            console.log(e);
+        });
+    }
+};
+
 export const ConfEditor = site_wrapper({
-    view: function () {
+    oninit() {
+        State.fetch();
+    },
+    view() {
         return m("div.home", [
-                m("h1", "Logs"),
-                m("div",
-                    m(DocEditor)
-                )
+                m("h1", "Configs"),
+                m("div", State.docComps)
             ]
         );
     }
--- src/development/scripts/components/confEditor/docEditor.js
+++ src/development/scripts/components/confEditor/docEditor.js
@@ -1,13 +1,73 @@
 import m from 'mithril';
+import {UploadSvg} from "../upload-icon/controller";
+import style from './doc_editor.scss';
 
-export const component_name = "DocEditor";
+export function docEditor(config) {
+    return class {
 
-export const DocEditor = {
-    view() {
-        return m("div.home", [
-                m("h1", "Config Editor"),
-                m("div.component_container", m('p', 'asdfasdf'))
-            ]
-        );
+        constructor(vnode) {
+            vnode.config = config;
+            vnode.text = JSON.stringify(config.document, null, 2);
+            vnode.setText = this.setText;
+            vnode.updateDocument = this.updateDocument;
+        }
+
+        updateDocument(e, vnode) {
+            console.log("update doc called");
+            try {
+                vnode.config.document = JSON.parse(vnode.text);
+                m.request({
+                    url: "/api/v1/documents",
+                    method: 'POST',
+                    data: vnode.config,
+                    extract: function (xhr) {
+                        return {status: xhr.status, body: xhr.responseText}
+                    }
+                }).then(function (response) {
+                    // let status = response.status;
+                    console.log(response)
+                })
+            } catch (e) {
+                console.log(e);
+                console.log('Refusing to send updated document. ' +
+                    `Reason: error occurred while parsing JSON in config '${vnode.config.name}'`)
+            }
+        }
+
+        setText(e, vnode) {
+            console.log("set text called");
+            console.log(vnode);
+            e.preventDefault();
+            vnode.text = e.currentTarget.value;
+            try {
+                vnode.config.document = JSON.parse(vnode.text);
+            } catch (e) {
+                console.log(`Error occurred while parsing JSON in config '${vnode.config.name}'`);
+            }
+        }
+
+        view(vnode) {
+            return m("div", [
+                m("h3", vnode.config.name),
+                m("textarea", {
+                    oninput: function (e) {
+                        vnode.setText(e, vnode)
+                    }
+                }, vnode.text),
+                m("div.bot",
+                    m("button.save-button", {
+                            onclick: function (e) {
+                                vnode.updateDocument(e, vnode)
+                            }
+                        },
+                        m("div", [
+                            m(UploadSvg),
+                            m("span", "Save")
+                        ])
+                    )
+                )
+            ]);
+        }
+
     }
-};
+}
