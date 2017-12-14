aec4164aaeeeaf880340ed4f315cb283fbbcda72
Harald Floor Wilhelmsen <harald.floor.wilhelmsen@trondheim.kommune.no>
2017-12-14 18:36:53 +0100

    state instance class. Service page now displays selects to select among all images which ever existed matching the service name and the environments the service is configured to run in. Clicking deploy will at this time result in a 404.

--- src/development/scripts/components/service/service.js
+++ src/development/scripts/components/service/service.js
@@ -1,67 +1,128 @@
 import m from 'mithril';
 import {map, pipe} from 'scripts/helpers/fp';
 import {site_wrapper} from "scripts/site";
+import style from './service.scss';
 
 export const component_name = "Service";
 
 
-const State = {
-    service_name: '',
-    data: {},
-    comps: m(''),
-    fetch: function () {
+class State {
+
+    constructor(vnode) {
+        this.selectedImage = '';
+        this.selectedEnv = '';
+        this.service_name = '';
+        this.data = {
+            environments: {},
+            'images-deployed': []
+        };
+    }
+
+    canDeploy() {
+        return this.selectedImage !== '' && this.selectedEnv !== '';
+    };
+
+    static setSelImage(img, vnode) { // this is too hacky, but until I find a better way it has to be this way
+        vnode.state.selectedImage = img;
+    };
+
+    static setSelEnv(env, vnode) {
+        console.log(vnode);
+        vnode.state.selectedEnv = env;
+    };
+
+    fetch() {
+        let state = this;
         m.request({
-            url: "/api/v1/service/" + State.service_name,
+            url: "/api/v1/service/" + this.service_name,
             method: 'GET',
         }).then(function (response) {
-            State.data = response;
-            State.parse(response);
+            state.data = response;
         }).catch(function (e) {
-            State.comps = m('p', e.message)
+            console.log(e.message)
         });
-    },
-    parse: function (data) {
-        let envs = data['environments'];
-        console.log(envs);
-        let rows = [
-            m('tr', [
-                m('th', 'Environment'),
-                m('th', 'Last deployed image'),
-                m('th', 'Deployed at')
-            ]),
-            pipe(Object.keys(envs),
-                map(k => {
-                        let epoch = envs[k]['time'];
-                        let timeString;
-                        if(!epoch){
-                            timeString = 'NA';
-                        } else {
-                            let date = new Date(0);
-                            date.setUTCSeconds(epoch);
-                            timeString = date.toLocaleString();
-                        }
+    };
 
-                        return m('tr', [
-                            m('td', k),
-                            m('td', envs[k]['image-name']),
-                            m('td', timeString)
-                        ])
-                    }
-                ), Array.from)
-        ];
-        State.comps = m('table', rows);
+    sendDeploy(vnode) {
+        let state = vnode.state;
+        m.request({
+            method: "POST",
+            url: "/api/v1/create/deploy",
+            data: {
+                environment: state.selectedEnv,
+                image: state.selectedImage
+            }
+        }).then(function (result) {
+            console.log(result)
+            // reset select fields
+            // create successful notification
+        }).catch(function (e) {
+            console.log(e.message);
+        });
     }
-};
+}
 
 export const Service = site_wrapper({
-    oninit() {
-        State.service_name = m.route.param('service');
-        State.fetch();
+    oninit(vnode) {
+        let state = new State();
+        state.service_name = m.route.param('service');
+        state.fetch();
+        vnode.state = state;
     },
     view(vnode) {
         return m("div.home", [
-                m("h1", State.service_name),
-                State.comps
+                m("h1", vnode.state.service_name),
+                m('div', [
+                    m('table', [
+                        m('tr', [
+                            m('th', 'Environment'),
+                            m('th', 'Last deployed image'),
+                            m('th', 'Deployed at')
+                        ]),
+                        pipe(Object.keys(vnode.state.data.environments),
+                            map(k => {
+                                    let epoch = vnode.state.data.environments[k]['time'];
+                                    let timeString;
+                                    if (!epoch) {
+                                        timeString = 'NA';
+                                    } else {
+                                        let date = new Date(0);
+                                        date.setUTCSeconds(epoch);
+                                        timeString = date.toLocaleString();
+                                    }
+
+                                    return m('tr', [
+                                        m('td', k),
+                                        m('td', vnode.state.data.environments[k]['image-name']),
+                                        m('td', timeString)
+                                    ])
+                                }
+                            ), Array.from)
+                    ]),
+                    m('div', [
+                        m('h3', 'Deploy'),
+                        m('div.deploy_grid', [
+                            m('select.deploy_select', {
+                                onchange: m.withAttr("value", (val) => State.setSelImage(val, vnode))
+                            }, [
+                                m('option', {value: ''}, 'Image'),
+                                pipe(vnode.state.data['images-deployed'],
+                                    map(img => m('option', {value: img}, img)), Array.from)
+                            ]),
+                            m('select.deploy_select', {
+                                onchange: m.withAttr("value", (val) => State.setSelEnv(val, vnode))
+                            }, [
+                                m('option', {value: ''}, 'Environment'),
+                                pipe(Object.keys(vnode.state.data.environments),
+                                    map(k => m('option', {value: k}, k)), Array.from)
+                            ]),
+                            m('button', {
+                                disabled: !vnode.state.canDeploy(),
+                                onclick: () => vnode.state.sendDeploy(vnode)
+                            }, 'Deploy')
+                        ])
+                    ])
+                ])
             ]
         );
     }
