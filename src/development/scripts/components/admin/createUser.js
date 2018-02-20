98e850f40839286b3a653fa740e21fb0b6e3fa77
Harald Floor Wilhelmsen <harald.floor.wilhelmsen@trondheim.kommune.no>
2018-02-20 12:11:13 +0100

    Permissions on user create/edit. Optimizations

--- src/development/scripts/components/admin/createUser.js
+++ src/development/scripts/components/admin/createUser.js
@@ -1,12 +1,14 @@
 import m from 'mithril';
 import {createNotification} from "../notifications/panel";
 import {req_with_auth} from 'scripts/helpers/requests';
+import {PermissionForm} from "../user/permission";
 
 export class CreateUserForm {
     constructor() {
         this.username = "";
         this.password = "";
         this.repeatPassword = "";
+        this.permissions = {};
     }
 
     setUsername(username) {
@@ -27,16 +29,19 @@ export class CreateUserForm {
         let password = this.password;
         let repeatPassword = this.repeatPassword;
         if (repeatPassword !== password) {
-            createNotification('Password mismatch', 'Passwords do not match', 'error');
+            createNotification('Password mismatch',
+                'Make sure password fields match', 'error');
             return;
         }
+        PermissionForm.removeEmptyPermissions(t.permissions);
         req_with_auth({
                 url: "/api/v1/create/user",
                 method: 'POST',
                 data: {
                     username: username,
                     password: password,
-                    'repeat-password': repeatPassword
+                    'repeat-password': repeatPassword,
+                    permissions: t.permissions
                 },
                 then: (e) => {
                     createNotification('Success', 'User was created', 'success');
@@ -47,7 +52,7 @@ export class CreateUserForm {
         );
     }
 
-    view() {
+    view(vnode) {
         return m('div', [
             m('h3', 'Create user'),
             m('label[for=username]', 'Username'),
@@ -65,7 +70,11 @@ export class CreateUserForm {
                 oninput: m.withAttr("value", this.setRepeatPassword, this),
                 placeholder: "Repeat Password"
             }),
-            m('button', {onclick: this.send.bind(this)}, 'Submit')
+            m('label', 'Permissions'),
+            m(PermissionForm, {permissions: this.permissions}),
+            m('button', {
+                onclick: m.withAttr("", this.send, this)
+            }, 'Submit')
         ])
     }
 }
