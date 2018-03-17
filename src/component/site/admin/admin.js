399b012d842bbff24c02d2799fa6f23a2751ae6e
Harald Floor Wilhelmsen <haraldfw@gmail.com>
2018-03-17 17:22:43 +0100

    Implemented masonry layout on admin page

--- src/component/site/admin/admin.js
+++ src/component/site/admin/admin.js
@@ -1,8 +1,10 @@
 import m from "mithril";
 import { site_wrapper } from "component/site/site-wrapper";
-import { Minimizable } from "component/minimizable/minimizable";
 import { CreateUserForm } from "./user-create";
 import { UserList } from "./user-list";
+import { MLContainer } from "component/masonry/ml-container";
+import { MLPanel } from "component/masonry/ml-panel";
+import { Minimizable } from "component/minimizable/minimizable";
 
 export const component_name = "Admin";
 export const Admin = site_wrapper({
@@ -10,14 +12,20 @@ export const Admin = site_wrapper({
         return m("div.admin",
             m("div.scroll", [
                 m("h1", "Admin"),
-                m(Minimizable, {
-                    comp: CreateUserForm,
-                    comp_title: "Create user"
-                }),
-                m(Minimizable, {
-                    comp: UserList,
-                    comp_title: "Existing users"
-                })
+                m(MLContainer, [
+                    m(MLPanel,
+                        m(Minimizable, {
+                            comp_title: "Create user",
+                            comp: CreateUserForm
+                        })
+                    ),
+                    m(MLPanel,
+                        m(Minimizable, {
+                            comp_title: "Existing Users",
+                            comp: UserList
+                        })
+                    )
+                ])
             ])
         );
     }
