acb44f427952a1645bf44cdb8f108047b15eb3d3
Harald Floor Wilhelmsen <haraldfw@gmail.com>
2018-03-19 12:20:40 +0100

    Changed events to show more info and to better match site theme

--- src/component/site/dashboard/events.js
+++ src/component/site/dashboard/events.js
@@ -1,5 +1,6 @@
 import m from "mithril";
 import { ErrorSvg } from "component/graphic/error/error";
+import { Checkmark } from "component/graphic/checkmark/checkmark";
 import { map, pipe } from "utils/fp";
 import { changefeed } from "services/api/reactive";
 import "rxjs-es/add/operator/debounceTime";
@@ -9,6 +10,28 @@ import { createNotification } from "../../notification/panel/panel";
 
 export const component_name = "Events";
 
+function taskSort(data, key1, key2) {
+    let status1 = data[key1].status;
+    let status2 = data[key2].status;
+    let time1 = data[key1].time;
+    let time2 = data[key2].time;
+
+    if(status1 === "processing" && status2 === "processing") {
+        return time2 - time1;
+    } else if(status1 === "processing") {
+        return -1;
+    } else if(status2 === "processing") {
+        return 1;
+    }
+
+    return time2 - time1;
+}
+
+const statusSvgMap = {
+    erroneous: ErrorSvg,
+    done:Checkmark
+};
+
 export const Events = {
     state: {
         socket_data: {}
@@ -16,7 +39,7 @@ export const Events = {
     oninit() {
 
         req_with_auth({
-            url: "/api/v1/tasks/recent"  + "?" + m.buildQueryString({
+            url: "/api/v1/tasks/recent" + "?" + m.buildQueryString({
                 amount: 15
             }),
             method: "GET",
@@ -44,25 +67,26 @@ export const Events = {
                 }
             });
     },
+
     view() {
         let data = this.state.socket_data;
         return m("div", [
             m("h3", "Events"),
             m("div.overview",
                 pipe(
-                    Object.keys(data).sort((key1, key2) => data[key2]['time'] - data[key1]['time']),
+                    Object.keys(data).sort((key1, key2) => taskSort(data, key1, key2)),
                     map(id => {
+                        console.log(data[id]);
                         let imageName = data[id]["image-name"];
                         let status = data[id]["status"];
-                        let icon;
-                        if (status === "erroneous") {
-                            icon = m("div.task-icon", m(ErrorSvg));
-                        } else {
-                            icon = m("div.task-icon");
-                        }
-                        return m("div.row.task-row." + status, [
-                            icon,
-                            m("span", imageName + " is " + status)
+                        return m("blockquote.event." + status, [
+                            status in statusSvgMap
+                                ? m("div.task-icon", m(statusSvgMap[status]))
+                                : m("div.task-icon"),
+                            data[id].environment
+                                ? m("span", imageName + " -> " + data[id]["environment"])
+                                : m("span", imageName),
+                            m("span.event-type", data[id]["event"])
                         ]);
                     }),
                     Array.from
