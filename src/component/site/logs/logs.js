e89d670d34c140ecc23769bcd8e06ee1cdf6525b
Harald Floor Wilhelmsen <harald.floor.wilhelmsen@trondheim.kommune.no>
2018-03-21 15:59:50 +0100

    Reformatting, ES-lint complainary continued

--- src/component/site/logs/logs.js
+++ src/component/site/logs/logs.js
@@ -27,40 +27,37 @@ function get_data_fields(row) {
 export const Logs = site_wrapper({
     view: function () {
         return m("div.home", [
-                m("h1", "Logs"),
-                m("div.scroll",
-                    m(Table, {
-                        resourceEndpoint: "/api/v1/tasks",
-                        pageStart: 0,
-                        pageLength: 20,
-                        pageLengthChoices: [1, 10, 20, 50, 100],
-                        headers: [
-                            ["time", true],
-                            "event",
-                            "status",
-                            "data"
-                        ],
-                        transformFunc(row) {
-                            let status = row["status"];
-                            let date = new Date(0);
-                            date.setUTCSeconds(row["time"]);
-                            let timeString = date.toLocaleString();
+            m("h1", "Logs"),
+            m("div.scroll", m(Table, {
+                resourceEndpoint: "/api/v1/tasks",
+                pageStart: 0,
+                pageLength: 20,
+                pageLengthChoices: [1, 10, 20, 50, 100],
+                headers: [
+                    ["time", true],
+                    "event",
+                    "status",
+                    "data"
+                ],
+                transformFunc(row) {
+                    let status = row["status"];
+                    let date = new Date(0);
+                    date.setUTCSeconds(row["time"]);
+                    let timeString = date.toLocaleString();
 
-                            let data = pipe(get_data_fields(row),
-                                map(key => row[key]),
-                                Array.from
-                            ).join(", ");
+                    let data = pipe(get_data_fields(row),
+                        map(key => row[key]),
+                        Array.from
+                    ).join(", ");
 
-                            return {
-                                cols: [timeString, row["event"], status, data],
-                                route: "/#!/log/" + row['id']
-                            };
-                        },
-                        rowClassFunc: row => row["status"] + " anchored"
-                    })
-                )
-            ]
-        );
+                    return {
+                        cols: [timeString, row["event"], status, data],
+                        route: "/#!/log/" + row["id"]
+                    };
+                },
+                rowClassFunc: row => row["status"] + " anchored"
+            }))
+        ]);
     },
     oncreate() {
         logsStyle.ref();
