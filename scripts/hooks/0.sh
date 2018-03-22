b14957a0f76c9f3388f1b9c9ea96741df4cce77b
Erlend Tobiassen (STUDET) <erlentob@stud.ntnu.no>
2018-03-22 15:13:03 +0100

    Version from develop not master and open github pull request

--- scripts/hooks/0.sh
+++ scripts/hooks/0.sh
@@ -3,7 +3,9 @@ function get_version {
 }
 
 function prehook {
-    [[ $(git rev-parse --abbrev-ref --symbolic-full-name @{u}) == "origin/master" ]] || err "Branch does not track origin/master"
+    git fetch
+    [[ $(git rev-parse --abbrev-ref --symbolic-full-name @{u}) == "origin/develop" ]] || err "Branch does not track origin/develop"
+    [[ $(git rev-parse HEAD) == $(git rev-parse @{u}) ]] || err "Branch is not up to date"
     git diff-files --quiet || err "Unstaged changes"
     git diff-index --quiet --cached HEAD -- || err "Changes not commited"
 }
@@ -17,8 +19,11 @@ function write_version {
 function posthook {
     git add package.json
     git commit -m "New $2 release version $1"
+    git checkout -b release/$1
+    git push --set-upstream origin/release/$1
+
     git tag $1
     git push origin $1
-    git checkout -b release/$1
-    git push --set-upstream
+
+    python -m webbrowser "https://github.com/cionkubes/cion-web/compare/master...release/$1?expand=1"
 }
